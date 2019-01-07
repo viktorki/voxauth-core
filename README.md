02e819e6ed0848caf2c749ea3928fa30fa7fe2b7
Viktor Ivanov <v.kr.ivanov@gmail.com>
2019-01-07 22:42:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# voxauth-core
---
+++ src/main/java/org/voxauth/voxauthcore/VoxauthCoreApplication.java
@@ -0,0 +1,14 @@
+package org.voxauth.voxauthcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxauthCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxauthCoreApplication.class, args);
+	}
+
+}
+
